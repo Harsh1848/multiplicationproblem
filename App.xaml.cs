04f31fd0e2dcb34e761694e11f2a7ddd45f7a0e5
Harsh1848 <harsh.hp7999@gmail.com>
2023-05-23 23:13:08 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace multiplicationproblem;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Maui.Controls;
+using System.Text;
+namespace multiplicationproblem;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void GenerateTableButton_Clicked(object sender, EventArgs e)
+    {
+        if (int.TryParse(NumberEntry.Text, out int number) && number >= -50 && number <= 50)
+        {
+            int upperLimit = 12;
+
+            if (int.TryParse(UpperLimitEntry.Text, out int customUpperLimit))
+            {
+                if (customUpperLimit > 0)
+                    upperLimit = customUpperLimit;
+                else
+                    DisplayAlert("Invalid Upper Limit", "Please enter a positive integer for the upper limit.", "OK");
+            }
+
+            StringBuilder tableBuilder = new StringBuilder();
+
+            for (int i = 1; i <= upperLimit; i++)
+            {
+                tableBuilder.Append($"{number} x {i} = {number * i}");
+                tableBuilder.AppendLine();
+            }
+
+            TableLabel.Text = tableBuilder.ToString();
+        }
+        else
+        {
+            DisplayAlert("Invalid Number", "Please enter a valid integer between -50 and 50.", "OK");
+        }
+    }
+}
+
